6e5519948e4d73325c64fc753ec8e360dd6d19b1
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-10 11:22:25 +0300

    added floataing action

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -42,6 +42,41 @@
             </div>
           </q-btn-dropdown>
         </div>
+        <div class="q-mt-md">
+          <q-fab
+            v-model="fab2"
+            label="Actions"
+            vertical-actions-align="left"
+            color="purple"
+            icon="keyboard_arrow_down"
+            direction="down"
+          >
+            <q-fab-action
+              color="primary"
+              @click="onClick"
+              icon="mail"
+              label="Email"
+            />
+            <q-fab-action
+              color="secondary"
+              @click="onClick"
+              icon="alarm"
+              label="Alarm"
+            />
+            <q-fab-action
+              color="orange"
+              @click="onClick"
+              icon="airplay"
+              label="Airplay"
+            />
+            <q-fab-action
+              color="accent"
+              @click="onClick"
+              icon="room"
+              label="Map"
+            />
+          </q-fab>
+        </div>
       </q-toolbar>
     </div>
     <AppFooter />
@@ -68,6 +103,7 @@ import AppFooter from "src/components/Footer.vue";
 const tab = ref("anasayfa");
 const mobileData = ref(false);
 const bluetooth = ref(false);
+const fab2 = ref(false);
 defineOptions({
   name: "MainLayout",
 });
