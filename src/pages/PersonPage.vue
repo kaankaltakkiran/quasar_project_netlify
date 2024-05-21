405348fdf5189aca279d1eb180be259beda00f18
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-05-21 18:26:03 +0300

    axios,pinia with data shown in exportable table

--- src/pages/PersonPage.vue
+++ src/pages/PersonPage.vue
@@ -1,31 +1,131 @@
 <template>
-  <q-page class="flex flex-center q-pa-md">
-    <q-card>
-      <q-card-section>
-        <q-list bordered padding>
-          <q-item v-for="user in users" :key="user.id">
-            <q-item-section>
-              <q-item-label>{{ user.name }}</q-item-label>
-              <q-item-label caption>{{ user.email }}</q-item-label>
-              <q-item-label caption>{{ user.phone }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-card-section>
-    </q-card>
-  </q-page>
+  <div class="row justify-center q-my-md">
+    <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+      <h2 class="text-center text-red">Person List</h2>
+      <q-page>
+        <q-card class="q-mt-md">
+          <q-card-section>
+            <q-table
+              flat
+              bordered
+              title="Person List"
+              :rows="users"
+              :columns="columns"
+              color="primary"
+              row-key="id"
+            >
+              <template v-slot:top-right>
+                <q-btn
+                  color="primary"
+                  icon-right="archive"
+                  label="Export to csv"
+                  no-caps
+                  @click="exportTable"
+                />
+              </template>
+            </q-table>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { useUserStore } from "src/stores/userStore";
 import { computed, onMounted } from "vue";
-
-// UserStore'u kullanarak kullanıcıları çekiyoruz
+import { exportFile, useQuasar } from "quasar";
+// storedan gelen verileri almak için
 const userStore = useUserStore();
-// Kullanıcıları computed ile alıyoruz
 const users = computed(() => userStore.users);
-// Veri çekme işlemini bileşen yüklendiğinde başlatmak için onMounted kullanıyoruz
+
+const $q = useQuasar();
+// tablo kolonları tanımlanır
+//name ve field alanları eşleşmelidir
+const columns = [
+  {
+    name: "id",
+    required: true,
+    label: "User Id",
+    align: "left",
+    field: "id",
+    sortable: true,
+  },
+  {
+    name: "name",
+    required: true,
+    label: "Name",
+    align: "left",
+    field: "name",
+    sortable: true,
+  },
+  {
+    name: "username",
+    required: true,
+    label: "User Name",
+    align: "left",
+    field: "username",
+    sortable: true,
+  },
+  {
+    name: "email",
+    required: true,
+    label: "Email",
+    align: "left",
+    field: "email",
+    sortable: true,
+  },
+];
+// mounted fonksiyonu ile sayfa yüklendiğinde userStore içerisindeki fetchUsers fonksiyonu çalıştırılır
 onMounted(() => {
   userStore.fetchUsers();
 });
+
+function wrapCsvValue(val, formatFn, row) {
+  let formatted = formatFn !== void 0 ? formatFn(val, row) : val;
+
+  formatted =
+    formatted === void 0 || formatted === null ? "" : String(formatted);
+
+  formatted = formatted.split('"').join('""');
+  return `"${formatted}"`;
+}
+
+function exportTable() {
+  const content = [columns.map((col) => wrapCsvValue(col.label))]
+    .concat(
+      users.value.map((row) =>
+        columns
+          .map((col) =>
+            wrapCsvValue(
+              typeof col.field === "function"
+                ? col.field(row)
+                : row[col.field === void 0 ? col.name : col.field],
+              col.format,
+              row
+            )
+          )
+          .join(",")
+      )
+    )
+    .join("\r\n");
+
+  const status = exportFile("table-export.csv", content, "text/csv");
+
+  if (status == true) {
+    $q.notify({
+      type: "positive",
+      message: "File downloaded successfully...",
+      color: "positive",
+      position: "top-right",
+    });
+  } else {
+    $q.notify({
+      message: "Browser denied file download...",
+      color: "negative",
+      icon: "warning",
+      position: "top-right",
+    });
+  }
+}
 </script>
